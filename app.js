1aab9ad744bfeb6727c7f7d9a8b951baf55f81a2
Haruki <harukixsira@gmail.com>
2017-05-11 16:35:25 -0500

    testing for heroku

--- app.js
+++ app.js
@@ -32,7 +32,8 @@ app.use(session({
     secret: 'replace me with long random string',
     resave : true,
     saveUninitialized: true,
-    // store: new MongoDBStore( {url: session_url })
+    store: new MongoDBStore( {uri: session_url,
+    collection: 'flower'})
 }));
 
 
